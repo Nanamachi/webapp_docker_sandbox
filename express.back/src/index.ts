61bdadeaf7063423a7a2abddfbad7a15a6a9b191
Nanamachi <7machi@nanamachi.net>
2020-04-05 05:48:30 +0900

    change express: add index.ts

---
+++ express.back/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+
+const app: express.Express = express()
+const port = 8081
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-requested-With, Content-Type, Accept")
+    next()
+})
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const router: express.Router = express.Router()
+
+router.get('/api/getTest', (req: express.Request, res: express.Response) => {
+    res.send(req.query)
+})
+
+app.use(router)
+app.listen(port, () => {console.log(`express.back listening on port ${port}.`)})
